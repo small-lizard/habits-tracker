09afd547c5a6148e7b57ccded95e5737dfe708a1
small-lizard <a.kulabukhova001@gmail.com>
2025-12-23 17:47:43 +0400

    fix: Fixed server-url

--- src/api/habitsApi.ts
+++ src/api/habitsApi.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 import { HabitOptions } from "../pages/HabitTracker/types";
 
-const API_URL = process.env.API_URL || "http://localhost:5000";
+const API_URL = process.env.NODE_ENV === 'development'
+  ? 'http://localhost:5000'
+  : '/api';
 
 export const getAllHabits = async () => {
     try {
