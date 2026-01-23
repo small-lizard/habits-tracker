c554a1a389491e6d7a2f944c89336323fdc64af0
small-lizard <a.kulabukhova001@gmail.com>
2026-01-23 14:55:36 +0400

    fix: Fixed habit display bug in calendar

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,8 @@ module.exports = {
   entry: "./src/index.tsx",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js"
+    filename: "bundle.js",
+    publicPath: "/",
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js"]
